a1e42c742e3d78cda18a6994ac2b903f3f011111
webwebweb5 <phiriyakornman@gmail.com>
2024-09-03 00:31:40 +0700

    feat: connect to database and add health check API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js"
+import connectDB from "./db/connectDB.js";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 8081;
@@ -12,6 +14,28 @@ app.get("/", (_req, res) => {
   res.send("API is running...");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/health", async (_req, res) => {
+  const dbState =
+    mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+
+  // Respond with health check information
+  res.status(200).json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    database: dbState,
+  });
 });
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
